fb2fd1eea83cf9c2400d968b224d3ce53b5d7140
Stephennu <stephen.tkn@gmail.com>
2014-01-19 19:05:58 -0500

    added CSS3 fading to images

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+var total_images = 3
+
+var current_image = 0;
+
 function change_image(id, src)
 {
 	document.getElementById(id).src = src;
 
+}
+
+function rotate_image()
+{
+	var results = "";
+	for (var current_image = 0; current_image <= total_images; current_image++)
+	{
+		results += "<img src=\""+current_image+".jpg\" />";
+	}
+	document.getElementById("rotating_images").innerHTML = results;
 }
